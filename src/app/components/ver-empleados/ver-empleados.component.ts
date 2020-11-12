ed8cc1a5081a9d5073020009bf2228208fa313e5
Miguel Ángel Ordóñez <ermigueog@gmail.com>
2020-11-12 13:58:55 +0100

    modifico columna de ver empleados

--- src/app/components/ver-empleados/ver-empleados.component.ts
+++ src/app/components/ver-empleados/ver-empleados.component.ts
@@ -14,6 +14,7 @@ export class VerEmpleadosComponent implements OnInit {
     'employeeName',
     'employeeSalary',
     'employeeAge',
+    'actions'
   ];
   dataSource: MatTableDataSource<EmployerCustom>;
     constructor() { }
